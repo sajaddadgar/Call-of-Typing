431e62f58d697191dd8b512da55bb6c13e9c4c64
s.dadgar <sajad.dadgar98@gmail.com>
2020-03-22 11:17:33 +0430

    Modify profile's model

--- call_of_typing/type/models.py
+++ call_of_typing/type/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Profile(models.Model):
-    pass
+    user =models.OneToOneField(User, on_delete=models.CASCADE)
+    max_point =models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
 
 
 class OrdinaryText(models.Model):
