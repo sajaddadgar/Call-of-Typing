a253228e8cb8883f36e81ea0fb7e12a6a5efe313
s.dadgar <sajad.dadgar98@gmail.com>
2020-03-23 17:50:16 +0430

    Fix some bugs

--- call_of_typing/call_of_typing/settings.py
+++ call_of_typing/call_of_typing/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cot',
         'USER': 'root',
-        'PASSWORD': '24762475',
+        'PASSWORD': 'java123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
