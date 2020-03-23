133e655b518c8c16c60d9bdb1400353a2657fc2e
amirsdg7798 <amirsadeghi7798@gmail.com>
2020-03-23 17:27:17 +0430

    type/urls.py updated Amir

--- call_of_typing/call_of_typing/settings.py
+++ call_of_typing/call_of_typing/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cot',
         'USER': 'root',
-        'PASSWORD': 'java123',
+        'PASSWORD': '24762475',
         'HOST': 'localhost',
         'PORT': '3306',
     }
