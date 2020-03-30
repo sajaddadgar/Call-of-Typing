56fd20846e764e5b4c289014cf198b34b43db50b
mohammad8921 <lashgari.m@live.com>
2020-03-30 14:57:26 +0430

    profile image bug fixed

--- call_of_typing/call_of_typing/urls.py
+++ call_of_typing/call_of_typing/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('type.urls')),
