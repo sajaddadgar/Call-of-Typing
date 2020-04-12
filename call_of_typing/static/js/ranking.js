198f17b688c730a4c617950233eada70e8ae5b5c
sh_hn <shideh.hashemian@gmail.com>
2020-04-12 23:03:58 +0430

    added song_ranking_table to ranking.html and updated ranking.js and ranking.css

--- call_of_typing/static/js/ranking.js
+++ call_of_typing/static/js/ranking.js
@@ -1,33 +1,53 @@
-function type_ranking(){
+function type_ranking() {
 
-    // alert('im there');
-    type_table_header = document.querySelector("#type-ranking-table-header");
-    type_table_body = document.querySelector("#type-ranking-table-body");
-    type_table_header.style.display='';
-    type_table_body.style.display='';
+    disappear_song_table();
+    appear_type_table();
+
 
 }
 
-function song_ranking(){
-    type_table_header = document.querySelector("#type-ranking-table-header");
-    type_table_body = document.querySelector("#type-ranking-table-body");
-    type_table_header.style.display='none';
-    type_table_body.style.display='none';
+function song_ranking() {
+
+    disappear_type_table();
+    appear_song_table();
 
 }
 
 
-function total_ranking(){
+function total_ranking() {
     type_table_header = document.querySelector("#type-ranking-table-header");
     type_table_body = document.querySelector("#type-ranking-table-body");
-    type_table_header.style.display='none';
-    type_table_body.style.display='none';
+    type_table_header.style.display = 'none';
+    type_table_body.style.display = 'none';
 
 }
-function group_ranking(){
+function group_ranking() {
     type_table_header = document.querySelector("#type-ranking-table-header");
     type_table_body = document.querySelector("#type-ranking-table-body");
-    type_table_header.style.display='none';
-    type_table_body.style.display='none';
+    type_table_header.style.display = 'none';
+    type_table_body.style.display = 'none';
 
+}
+
+function appear_type_table() {
+
+    table = document.querySelector("#type-table");
+    table.style.display = '';
+}
+
+function disappear_type_table() {
+
+    table = document.querySelector("#type-table");
+    table.style.display = 'none';
+}
+
+function appear_song_table() {
+
+    table = document.querySelector("#song-table");
+    table.style.display = '';
+}
+function disappear_song_table() {
+
+    table = document.querySelector("#song-table");
+    table.style.display = 'none';
 }
