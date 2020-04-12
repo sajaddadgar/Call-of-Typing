a84e2f69a69559afcafed2118a794d097d305e0e
sh_hn <shideh.hashemian@gmail.com>
2020-04-12 20:03:56 +0430

    login.html, login.css updated. newLogin.html deleted. in views.py in singin function, 'passw' replaced with 'password'

---
+++ call_of_typing/static/js/ranking.js
@@ -0,0 +1,33 @@
+function type_ranking(){
+
+    // alert('im there');
+    type_table_header = document.querySelector("#type-ranking-table-header");
+    type_table_body = document.querySelector("#type-ranking-table-body");
+    type_table_header.style.display='';
+    type_table_body.style.display='';
+
+}
+
+function song_ranking(){
+    type_table_header = document.querySelector("#type-ranking-table-header");
+    type_table_body = document.querySelector("#type-ranking-table-body");
+    type_table_header.style.display='none';
+    type_table_body.style.display='none';
+
+}
+
+
+function total_ranking(){
+    type_table_header = document.querySelector("#type-ranking-table-header");
+    type_table_body = document.querySelector("#type-ranking-table-body");
+    type_table_header.style.display='none';
+    type_table_body.style.display='none';
+
+}
+function group_ranking(){
+    type_table_header = document.querySelector("#type-ranking-table-header");
+    type_table_body = document.querySelector("#type-ranking-table-body");
+    type_table_header.style.display='none';
+    type_table_body.style.display='none';
+
+}
